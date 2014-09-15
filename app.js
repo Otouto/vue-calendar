215d343268046279cd854c7da3ae7fab2279d44d
Dmitry Lutsik <otoyto@gmail.com>
2014-09-15 11:27:58 +0300

    up pages

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname));
-
-var server = app.listen(3005, function () {
-    console.log('Listening on port %d', server.address().port);
-});
